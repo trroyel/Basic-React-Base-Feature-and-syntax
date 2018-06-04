183f1c39b6e81037c97edd7edb4f6c89ef542953
trroyel <t.r.royel@gmail.com>
2018-06-04 16:06:01 +0600

    Using state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends PureComponent {
         { id: 'hsgd', name: 'Manu', age: 29 },
         { id: 'abcd', name: 'Stephanie', age: 26 }
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -90,7 +91,12 @@ class App extends PureComponent {
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
 
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   };
 
   render() {
